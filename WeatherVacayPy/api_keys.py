bf64db439e234e4a85a3d75aafe20513b662a4b2
Geniya-M <geniyabranch@gmail.com>
2024-11-15 00:52:14 -0500

    api_keys.py

--- WeatherVacayPy/api_keys.py
+++ WeatherVacayPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "487f19b88e730d357ea5ce6daa8eb1b0"
+weather_api_key = ""
 
 # Geoapify API Key
-geoapify_key = "fe3da74c701544888a69114ab9119dd5"
+geoapify_key = ""
